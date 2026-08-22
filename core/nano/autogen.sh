54bb7a6980a44f81e7096bfa2885a0e0e8a8ccd0
siddharthantv <siddharth13102@gmail.com>
2026-08-22 15:01:19 +0530

    Automated mirror sync: core/nano - 2026-08-22

---
+++ core/nano/autogen.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+# Generate configure & friends for GIT users.
+
+gnulib_url="git://git.sv.gnu.org/gnulib.git"
+gnulib_hash="833eea6be5607a85bc964bf728a7a82ca461b5b1"
+
+modules="
+	canonicalize-lgpl
+	futimens
+	getdelim
+	getline
+	getopt-gnu
+	glob
+	isblank
+	iswblank
+	lstat
+	mkstemps
+	nl_langinfo
+	regex
+	sigaction
+	snprintf-posix
+	stdarg-h
+	strcase
+	strcasestr-simple
+	strnlen
+	sys_wait
+	vsnprintf-posix
+	wchar-h
+	wctype-h
+	wcwidth
+"
+
+# Make sure the local gnulib git repo is up-to-date.
+if [ ! -d "gnulib" ]; then
+	git clone --depth=2222 ${gnulib_url}
+fi
+cd gnulib >/dev/null || exit 1
+curr_hash=$(git log -1 --format=%H)
+if [ "${gnulib_hash}" != "${curr_hash}" ]; then
+	echo "Pulling..."
+	git pull
+	git checkout --force ${gnulib_hash}
+fi
+cd .. >/dev/null || exit 1
+
+rm -rf lib
+echo "Gnulib-tool..."
+./gnulib/gnulib-tool --import ${modules}
+echo
+
+echo "Autoreconf..."
+autoreconf --install --symlink --force
+echo "Done."
